0b4ea0922c414e39b97deceb80d86d572f50c268
PedroTra <151466351+PedroTra@users.noreply.github.com>
2024-09-22 17:46:10 -0300

    Create Atividades.java

---
+++ Atividades.java
@@ -0,0 +1,23 @@
+
+public interface Atividades {
+
+    /**
+     * Método para acordar a pessoa.
+     */
+    void acordar();
+
+    /**
+     * Método para a pessoa andar.
+     */
+    void andar();
+
+    /**
+     * Método para a pessoa comer.
+     */
+    void comer();
+
+    /**
+     * Método para a pessoa dormir.
+     */
+    void dormir();
+}
